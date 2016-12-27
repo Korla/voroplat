41f2ffc209c1be5b5440567ffec0c07d8e2541b9
korla <johannes.bengtsson@raysearchlabs.com>
2016-12-27 15:54:33 +0100

    Add clock to vertexShader, batch render with single draw call

--- src/index3.js
+++ src/index3.js
@@ -1,6 +1,6 @@
 (function(){
-  var fragments = 10;
-  var coneRadius = 30;
+  var fragments = 50;
+  var coneRadius = 50;
 
   var mainCanvas = document.getElementById('main-canvas');
 
@@ -30,93 +30,137 @@
 
   gl.useProgram(shaderProgram);
 
-  // Set up cone- and color arrays
-  var getNextColor = colorFactory(1000);
-  var degToRad = Math.PI / 180.0;
-  var degInc = 360.0/fragments;
-  var coneHeight = coneRadius / Math.tan(45 * degToRad);
-
-  var count = 100;
-  var colors = [];
-  var positions = [];
-  var worldPositions = [];
-  var t1 = performance.now();
-  for(var x = (width/count)/2; x < width; x += width/count) {
-    var rowPositions = [];
-    var rowWorldPositions = [];
-    var rowColors = [];
-    for(var y = (height/count)/2; y < height; y += height/count) {
-      var color = getNextColor();
-      var worldPosition = [x, y, 0];
-
-      var elementPositions = [];
-      var elementWorldPositions = [];
-      var elementColors = [];
-
-      var curDeg = 0;
-      for(var i = 0; i < fragments; i++) {
-        elementPositions = elementPositions.concat([0,0,0]);
-        elementWorldPositions = elementWorldPositions.concat(worldPosition);
-        elementColors = elementColors.concat(color);
-
-        elementPositions = elementPositions.concat([
-          coneRadius * Math.cos(curDeg * degToRad),
-          coneRadius * Math.sin(curDeg * degToRad),
-          -1.0 * coneHeight
-        ]);
-        elementWorldPositions = elementWorldPositions.concat(worldPosition);
-        elementColors = elementColors.concat(color);
-
-        elementPositions = elementPositions.concat([
-          coneRadius * Math.cos((curDeg + degInc) * degToRad),
-          coneRadius * Math.sin((curDeg + degInc) * degToRad),
-          -1.0 * coneHeight
-        ]);
-        elementWorldPositions = elementWorldPositions.concat(worldPosition);
-        elementColors = elementColors.concat(color);
-
-        curDeg += degInc;
-      }
-
-      rowPositions = rowPositions.concat(elementPositions);
-      rowWorldPositions = rowWorldPositions.concat(elementWorldPositions);
-      rowColors = rowColors.concat(elementColors);
-    }
-
-    positions = positions.concat(rowPositions);
-    worldPositions = worldPositions.concat(rowWorldPositions);
-    colors = colors.concat(rowColors);
-  }
-  console.log(performance.now() - t1);
-
-  console.log(colors.length / 4, worldPositions.length / 3, positions.length / 3, count * count * fragments);
-  console.log({colors, worldPositions, positions});
-
-  // Set up buffers
-  var positionBuffer = createArrayBuffer('position', 3, positions);
-  var colorBuffer = createArrayBuffer('color', 4, colors);
-  var worldPositionBuffer = createArrayBuffer('worldPosition', 3, worldPositions);
-
   //Gui init
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
+  gl.disable(gl.BLEND);
 
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   var viewProjection = new Float32Array(makeOrtho(0, gl.viewportWidth, gl.viewportHeight, 0, -5, 5000).flatten());
-  shaderProgram.viewProjection = gl.getUniformLocation(shaderProgram, 'viewProjection');
-  gl.uniformMatrix4fv(shaderProgram.viewProjection, false, viewProjection);
-  gl.disable(gl.BLEND);
 
+  // Set up cone- and color arrays
+  var green = () => [0.1 + Math.random() * 0.2,0.4,0.1 + Math.random() * 0.2,1];
+  var blue = ratio => [ratio,ratio/2,0.5,1];
+  var degToRad = Math.PI / 180.0;
+  var degInc = 360.0/fragments;
+  var coneHeight = coneRadius / Math.tan(45 * degToRad);
+
+  var count = 30;
+
+  var createWorldData = worldDataCreator([
+    'positions',
+    'positionDisplacements',
+    'colors',
+    'worldPositions',
+    'clockOffsets',
+    'clockSpeeds',
+    'radiuses'
+  ]);
+
+  var data = createWorldData();
+
+  var t1 = performance.now();
+  for(var x = (width/count)/2; x < width; x += width/count) {
+    var rowData = createWorldData();
+
+    for(var y = (height/count)/2; y < height; y += height/count) {
+      var color = y / height < 0.8 ? blue(y / height) : green()
+      var worldPosition = [x, y, 0];
+      var positionDisplacement = [Math.random()*5, Math.random()*5, 0];
+      var clockOffset = [0, 0, 0];
+      var clockSpeed = [Math.random()/300, Math.random()/300, 0];
+      var radius = [Math.random()*10, Math.random()*10, 0];
+
+      var elementData = createWorldData();
+
+      var curDeg = 0;
+      for(var i = 0; i < fragments; i++) {
+        concatenateData(
+          elementData,
+          {
+            positions: [0, 0, 0],
+            positionDisplacements: positionDisplacement,
+            worldPositions: worldPosition,
+            colors: color,
+            clockOffsets: clockOffset,
+            clockSpeeds: clockSpeed,
+            radiuses: radius
+          }
+        );
+
+        concatenateData(
+          elementData,
+          {
+            positions: [
+              coneRadius * Math.cos(curDeg * degToRad),
+              coneRadius * Math.sin(curDeg * degToRad),
+              -1.0 * coneHeight
+            ],
+            positionDisplacements: positionDisplacement,
+            worldPositions: worldPosition,
+            colors: color,
+            clockOffsets: clockOffset,
+            clockSpeeds: clockSpeed,
+            radiuses: radius
+          }
+        );
+
+        concatenateData(
+          elementData,
+          {
+            positions: [
+              coneRadius * Math.cos((curDeg + degInc) * degToRad),
+              coneRadius * Math.sin((curDeg + degInc) * degToRad),
+              -1.0 * coneHeight
+            ],
+            positionDisplacements: positionDisplacement,
+            worldPositions: worldPosition,
+            colors: color,
+            clockOffsets: clockOffset,
+            clockSpeeds: clockSpeed,
+            radiuses: radius
+          }
+        );
+
+        curDeg += degInc;
+      }
+
+      concatenateData(rowData, elementData);
+    }
+
+    concatenateData(data, rowData);
+  }
+  console.log(performance.now() - t1);
+  console.log(data);
+
+  var {positions, positionDisplacements, colors, worldPositions, clockOffsets, clockSpeeds, radiuses} = data;
+
+  // Set up buffers
+  var positionBuffer = createArrayBuffer('position', 3, positions);
+  var positionDisplacementBuffer = createArrayBuffer('positionDisplacement', 3, positionDisplacements);
+  var colorBuffer = createArrayBuffer('color', 4, colors);
+  var worldPositionBuffer = createArrayBuffer('worldPosition', 3, worldPositions);
+  var clockOffsetBuffer = createArrayBuffer('clockOffset', 3, clockOffsets);
+  var clockSpeedBuffer = createArrayBuffer('clockSpeed', 3, clockSpeeds);
+  var radiusBuffer = createArrayBuffer('radius', 3, radiuses);
+
+  gl.uniformMatrix4fv(gl.getUniformLocation(shaderProgram, 'viewProjection'), false, viewProjection);
+  var clockLocation = gl.getUniformLocation(shaderProgram, 'clock');
+  gl.uniform1f(clockLocation, 10.0);
+
+  // Render
+  var positionsToRender = new Float32Array(positions);
+  var numberOfTrianglesToRender = positionsToRender.length / 3;
   function loop(timestamp) {
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    gl.drawArrays(gl.TRIANGLES, 0, positions.length / 3);
+    gl.uniform1f(clockLocation, timestamp);
+    gl.drawArrays(gl.TRIANGLES, 0, numberOfTrianglesToRender);
     window.requestAnimationFrame(loop);
   };
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionDisplacementBuffer);
   window.requestAnimationFrame(loop);
 
   function getShader(gl, id) {
@@ -175,6 +219,19 @@
       var colorIndex = currCol % colors.length;
       currCol++;
       return colors[colorIndex];
+    }
+  }
+
+  function worldDataCreator(props) {
+    return () => props.reduce((data, propertyName) => {
+      data[propertyName] = [];
+      return data;
+    }, {});
+  }
+
+  function concatenateData(data, data2) {
+    for(var key of Object.keys(data)) {
+      data[key] = data[key].concat(data2[key]);
     }
   }
 
